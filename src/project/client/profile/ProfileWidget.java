2c77f659e7fb87b1e94c16fd338d436ec5d1c3be
Patrick Nguyen <pahdnguyen@yahoo.com>
2012-08-10 11:33:39 -0700

    Added Labels to profile page

--- src/project/client/profile/ProfileWidget.java
+++ src/project/client/profile/ProfileWidget.java
@@ -2,15 +2,18 @@ package project.client.profile;
 
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.gen2.picker.client.SliderBar;
+import com.google.gwt.user.client.ui.Label;
+
 import project.client.login.LoginInfo;
 import project.client.screen.ScreenWidget;
 
 public class ProfileWidget extends ScreenWidget {
-	private SliderBar main = createSlider("User Story");
-	private SliderBar ePoint = createSlider("Entry Point");
-	private SliderBar sketch = createSlider("Sketch/Impl");
-	private SliderBar testCase = createSlider("Test Case");
-	private SliderBar unit = createSlider("Unit Test");
+	private SliderWidget main = new SliderWidget("User Story");
+	private SliderWidget ePoint = new SliderWidget("Entry Point");
+	private SliderWidget sketch = new SliderWidget("Sketch/Impl");
+	private SliderWidget testCase = new SliderWidget("Test Case");
+	private SliderWidget unit = new SliderWidget("Unit Test");
+	
 	public ProfileWidget(LoginInfo loginInfo) {
 		super(loginInfo);
 		setSize("1150px", "768px");
@@ -20,48 +23,46 @@ public class ProfileWidget extends ScreenWidget {
 
 	@Override
 	public void UI() {
-
-		
 		mainPanel.add(main);
 		mainPanel.add(ePoint);
 		mainPanel.add(sketch);
 		mainPanel.add(testCase);
 		mainPanel.add(unit);
-		//mainPanel.setWidgetLeftWidth(main, 100, Unit.PX, 100, Unit.PX); 
-		mainPanel.setWidgetTopHeight(main, 100, Unit.PX, 100, Unit.PX); 
 		
-		//mainPanel.setWidgetLeftWidth(ePoint, 100, Unit.PX, 100, Unit.PX); 
+
+		mainPanel.setWidgetTopHeight(main, 100, Unit.PX, 100, Unit.PX);
+		
 		mainPanel.setWidgetTopHeight(ePoint, 200, Unit.PX, 100, Unit.PX); 
 		
-		//mainPanel.setWidgetLeftWidth(sketch, 100, Unit.PX, 100, Unit.PX); 
 		mainPanel.setWidgetTopHeight(sketch, 300, Unit.PX, 100, Unit.PX); 
 		
-		//mainPanel.setWidgetLeftWidth(testCase, 100, Unit.PX, 100, Unit.PX); 
 		mainPanel.setWidgetTopHeight(testCase, 400, Unit.PX, 100, Unit.PX); 
 		
-		//mainPanel.setWidgetLeftWidth(unit, 100, Unit.PX, 100, Unit.PX); 
 		mainPanel.setWidgetTopHeight(unit, 500, Unit.PX, 100, Unit.PX); 
-		}
-	
-	public SliderBar createSlider(String name){
-		  SliderBar slider = new SliderBar(0.0, 100.0);
-		  slider.setStepSize(2.0);
-		  slider.setCurrentValue(50.0);
-		  slider.setNumTicks(0);
-		  slider.setNumLabels(2);
-		  slider.setHeight("50px");
-		  return slider;
+		
 	}
+	
+	
 		
 
 	@Override
 	public void submit() {
-		main.getCurrentValue();
-		ePoint.getCurrentValue();
-		sketch.getCurrentValue();
-		testCase.getCurrentValue();
-		unit.getCurrentValue();
-		
+		if(main==null)
+			System.out.println("null1");
+		else System.out.println(main.getCurrentValue());
+		if(ePoint==null)
+			System.out.println("null2");
+		else System.out.println(ePoint.getCurrentValue());
+		if(sketch==null)
+			System.out.println("null3");
+		else System.out.println(sketch.getCurrentValue());
+		if(testCase==null)
+			System.out.println("null4");
+		else System.out.println(testCase.getCurrentValue());
+		if(unit==null)
+			System.out.println("null5");
+		else System.out.println(unit.getCurrentValue());
+		System.out.println();
 	}
 	
 
--- src/project/client/profile/SliderWidget.java
+++ src/project/client/profile/SliderWidget.java
@@ -1,7 +1,54 @@
 package project.client.profile;
 
-import com.kiouri.sliderbar.client.view.SliderBarHorizontal;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.gen2.picker.client.SliderBar;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
 
-public class SliderWidget{
-
+public class SliderWidget extends LayoutPanel{
+	private SliderBar slider;
+	private Label label;
+	
+	private final int height=50;
+	private final int sliderWidth=300;
+	private final int labelWidth=200;
+	public SliderWidget(String microtask){
+		
+		label=new Label("Preference for "+microtask+" tasks");
+		slider=createSlider();
+		
+		
+		add(label);
+		setWidgetTopHeight(label, height/3, Unit.PX, height, Unit.PX);
+		setWidgetLeftWidth(label, 0, Unit.PX, labelWidth, Unit.PX);
+		
+		add(slider);
+		setWidgetTopHeight(slider, 0, Unit.PX, height, Unit.PX);
+		setWidgetLeftWidth(slider, 300, Unit.PX, sliderWidth, Unit.PX);
+		
+	}
+	
+	private SliderBar createSlider(){
+		  SliderBar slider = new SliderBar(0.0, 100.0){
+			  protected String formatLabel(double val){
+				  //System.out.println(super.formatLabel(val)+"\n");
+				  /*if(val==0)
+					  return "I hate it";
+				  if(val==100)
+					  return "*/
+				  return super.formatLabel(val);
+			  }
+		  };
+		  slider.setStepSize(2.0);
+		  slider.setCurrentValue(50.0);
+		  slider.setNumTicks(0);
+		  slider.setNumLabels(2);
+		  slider.setHeight(height+"px");
+		  slider.setWidth(sliderWidth+"px");
+		  return slider;
+	}
+	
+	public double getCurrentValue(){
+		return slider.getCurrentValue();
+	}
 }
